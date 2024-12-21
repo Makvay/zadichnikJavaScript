d24195692a6e64554ca9143fe0b6a9cb6b05dfa1
Makvay <komboclash373@gmail.com>
2024-12-21 21:04:50 +0800

    1

---
+++ main.js
@@ -0,0 +1,14 @@
+// let min = 50;
+//
+// if (min >= 0 && min <= 20) {
+//     console.log('1 треть');
+// }
+//
+// if (min >= 21 && min <= 40) {
+//     console.log('2 терть');
+// }
+//
+// if (min >= 41 && min <= 60) {
+//     console.log('3 треть');
+// }
+//
